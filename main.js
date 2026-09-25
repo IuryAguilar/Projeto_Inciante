d0c858c77a9821031f075585722fddd06060e837
Iury Aguilar <iuryadeles@gmail.com>
2026-09-24 21:16:09 -0300

    Experimentando calculadora

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
+        resizable: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
--- src/views/renderer.js
+++ src/views/renderer.js
@@ -1,6 +1,7 @@
-const information = document.getElementById('info')
 
-information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
+console.log(`Chrome (v${window.versions.chrome()})`)
+console.log(`Node.js (v${window.versions.node()})`) 
+console.log(`Electron (v${window.versions.electron()})`)
 
 const func = async () => {
     const response = await window.versions.ping()
